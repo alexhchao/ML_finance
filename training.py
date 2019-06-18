a5ab67c1573954cfdfd60a67385cc6616ffd30ee
Alex <alexchao86@gmail.com>
2019-06-17 23:36:07 -0400

    added stats calcs

--- training.py
+++ training.py
@@ -22,6 +22,7 @@ df
 #############################
 # first build a light weight backtester
 #############################
+order = 5
 
 n = 5
 
@@ -31,9 +32,16 @@ df_2 = add_quintiles_as_new_col(df,
                              groupby_col_name = 'date',
                                 n = 5)
 
+df_2.dtypes
+
 df_2.groupby('date').mom_q.value_counts()
 
-avg_rets = df_2.groupby(['mom_q','date'])['fwd_returns'].mean().unstack().T
+avg_rets = df_2.groupby(['mom_q','date'])['fwd_returns'].mean().unstack().T.shift(1) * 0.01
+
+# Long minus short basket
+if order = 'ascending':
+    avg_rets['LS - {} minus {}'.format()]
+
 
 def calc_sharpe(x,
                 n=12):
@@ -41,8 +49,51 @@ def calc_sharpe(x,
 
 avg_rets.apply(calc_sharpe)
 
-np.cumprod(1+(avg_rets.unstack().T*0.01)).plot()
+_ret = avg_rets.iloc[:,0]
 
+calc_stats(_ret/100)
+
+# okay this gets stats for each basket
+all_stats = avg_rets.apply(lambda x: pd.Series(calc_stats(x)))
+
+wealth = np.cumprod(1+avg_rets)
+
+wealth.plot()
+
+#{calc_stats(avg_rets.loc[:,x]) for x in list(avg_rets.columns)}
+
+
+
+_ret.index[0]
+_ret.index[-1]
+
+
+def calc_stats(_ret_series,
+               n=12 # for monthly
+                 ):
+    """
+    
+    Parameters
+    ----------
+    _ret_series
+
+    Returns
+    -------
+
+    """
+    _stats = {}
+
+    adj_factor = np.sqrt(n)
+    num_obs = _ret_series.shape[0]
+
+    _stats['rets'] = _ret_series.mean() * n
+    _stats['vol'] = _ret_series.std() * adj_factor
+    _stats['sharpe']=_stats['rets'] /_stats['vol']
+    _stats['tstat'] = _stats['sharpe'] * np.sqrt(num_obs) / adj_factor
+    _stats['start_dt'] = _ret_series.index[0]
+    _stats['end_dt'] = _ret_series.index[-1]
+    #import pdb; pdb.set_trace()
+    return _stats
 
 
 
@@ -65,7 +116,7 @@ def add_quintiles_as_new_col(df,
     _df = df.copy()
 
     _df[new_col_name] = _df.groupby(groupby_col_name)[col_name].transform(lambda x: pd.qcut(
-        x, q = n, labels = np.arange(1,n+1))).astype(float)
+        x, q = n, labels = np.arange(1,n+1), precision=0)).astype(str)
     return _df
 
 
