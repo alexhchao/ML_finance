e8ee52c4ca83ffca9c6f9348b014779631aa0ba7
Alex <alexchao86@gmail.com>
2019-06-18 21:30:55 -0400

    added SignalUnivariateStudy class

--- training.py
+++ training.py
@@ -22,8 +22,7 @@ df
 #############################
 # first build a light weight backtester
 #############################
-order = 5
-
+order = 'ascending'
 n = 5
 
 df_2 = add_quintiles_as_new_col(df,
@@ -32,20 +31,39 @@ df_2 = add_quintiles_as_new_col(df,
                              groupby_col_name = 'date',
                                 n = 5)
 
+df_2['mom_q'] = df_2.groupby('date')['momentum'].transform(lambda x: pd.qcut(
+        x, q=n, labels= [str(x).split('.')[0] for x in np.arange(1,n+1)]  )).astype(
+            str).apply(lambda x: x.split('.')[0])
+
+x = df_2.groupby(['date','sector']).momentum.percentile_rank()
+
+df = add_sector_neutral_column(df = df,
+                          col_to_neutralize = 'momentum',
+                          neutralized_col_name=None,
+                          agg_col_names = ['date','sector'])
+
+
+df.query("date == '2006-01-31'").query("momentum.notnull()", engine = 'python').query("sector==2.0")
+
+
+x = df_2.groupby(['date','sector']).momentum.rank(pct=True)
+
+x.dropna()
+
+
+df_2['mom_q'].replace('nan',np.NaN, inplace = True)
+
+#df_2['mom_q'] = df_2['mom_q'].apply(lambda x: x.split('.')[0])
+
 df_2.dtypes
 
 df_2.groupby('date').mom_q.value_counts()
 
-avg_rets = df_2.groupby(['mom_q','date'])['fwd_returns'].mean().unstack().T.shift(1) * 0.01
 
 # Long minus short basket
 if order = 'ascending':
     avg_rets['LS - {} minus {}'.format()]
 
-
-def calc_sharpe(x,
-                n=12):
-    return x.mean()* np.sqrt(n) / x.std()
 
 avg_rets.apply(calc_sharpe)
 
@@ -53,12 +71,17 @@ _ret = avg_rets.iloc[:,0]
 
 calc_stats(_ret/100)
 
-# okay this gets stats for each basket
-all_stats = avg_rets.apply(lambda x: pd.Series(calc_stats(x)))
 
-wealth = np.cumprod(1+avg_rets)
+rets = df_2.groupby(['mom_q','date'])['fwd_returns'].mean().unstack().T.shift(1) * 0.01
+#rets.loc[:,'1']
+
+# okay this gets stats for each basket
+all_stats = rets.apply(lambda x: pd.Series(calc_stats(x)))
+
+wealth = np.cumprod(1+rets)
 
 wealth.plot()
+
 
 #{calc_stats(avg_rets.loc[:,x]) for x in list(avg_rets.columns)}
 
@@ -67,6 +90,26 @@ wealth.plot()
 _ret.index[0]
 _ret.index[-1]
 
+
+############################
+
+
+sig = SignalUnivariateStudy(data_df = df,
+                            factor_name = 'vol',
+                            neutralizer_column = 'sector',
+                            order = 'asc',
+                            n = 5)
+sig
+
+sig.wealth.plot()
+
+df['fwd_returns'].mean()
+
+print(sig.stats)
+
+pd.DataFrame(sig.stats)
+
+############################
 
 def calc_stats(_ret_series,
                n=12 # for monthly
